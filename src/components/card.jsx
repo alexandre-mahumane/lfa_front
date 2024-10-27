d8106b47209c86d0b95a84800a1fae93291bc16d
AlexandreMahumane <alexandremahumane69@gmail.com>
2024-10-27 08:09:50 -0700

    set: card component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,8 +1,9 @@
 import photo from "../assets/photo5.jpeg";
 import { LinkComponent } from "./link-component";
 import { motion } from "framer-motion";
+
 export const Card = ({ variant, path, pic, title, text }) => {
-  const type = `${variant != "N" ? "px-0" : "px-3"}`;
+  const type = `${variant !== "N" ? "px-0" : "px-3"}`;
   return (
     <motion.section
       initial={{ opacity: 0, y: 50 }}
@@ -14,9 +15,11 @@ export const Card = ({ variant, path, pic, title, text }) => {
       <div className="h-56">
         <img src={pic} className="w-full object-cover h-full" alt="" />
       </div>
-      <div>
-        <h3 className={`text-lg ${type} px-3 font-medium`}>{title}</h3>
-        <p className="font-thin text-sm">{text}</p>
+      <div className="space-y-2 mt-3">
+        <h3 className={`text-lg ${type} px-3 font-bold truncate`} title={title}>
+          {title}
+        </h3>
+        <p className="font-thin text-sm line-clamp-3">{text}</p>
       </div>
       <LinkComponent path={path} />
     </motion.section>
