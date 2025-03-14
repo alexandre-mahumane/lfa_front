ec765773667ea70168818e0863360431f855857b
AlexandreMahumane <alexandremahumane69@gmail.com>
2025-03-14 20:19:12 +0200

    update: news letter card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import photo from "../assets/photo5.jpeg";
 import { LinkComponent } from "./link-component";
 import { motion } from "framer-motion";
@@ -12,18 +13,20 @@ export const Card = ({ variant, path, pic, title, text }) => {
       viewport={{ once: true }}
       className="p-4 h-fit  w-72 md:w-full border-b border-black"
     >
-      <div className="h-56 md:h-96 w-full">
-        <img src={pic} className="w-full object-cover h-full" alt="" />
-      </div>
-      <div className="space-y-2 mt-3">
-        <h3
-          className={`text-lg ${type}  px-3 font-bold truncate`}
-          title={title}
-        >
-          {title}
-        </h3>
-        <p className="font-thin text-sm line-clamp-3">{text}</p>
-      </div>
+      <Link to={`information/${path}`}>
+        <div className="h-56 md:h-96 w-full">
+          <img src={pic} className="w-full object-cover h-full" alt="" />
+        </div>
+        <div className="space-y-2 mt-3">
+          <h3
+            className={`text-lg ${type}  px-3 font-bold truncate`}
+            title={title}
+          >
+            {title}
+          </h3>
+          <p className="font-thin text-sm line-clamp-3">{text}</p>
+        </div>
+      </Link>
       <LinkComponent path={path} />
     </motion.section>
   );
